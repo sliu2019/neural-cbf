before changing incorrect computation of higher-order Lie derivatives from Pascal-based to the correct approach

main.py
@@ -3,13 +3,14 @@ import numpy as np
 
 from torch import nn
 from torch.autograd import grad
-from attacks.basic import BasicAttacker
+from src.attacks.basic_attacker import BasicAttacker
 from src.trainer import Trainer
 from src.utils import *
 from scipy.linalg import pascal
 from src.argument import parser, print_args
 import os, sys
 import math
+import IPython
 
 class Phi(nn.Module):
 	# TODO: get phi_i for all i from here. Use forward hooks
@@ -23,9 +24,9 @@ class Phi(nn.Module):
 		del self.__dict__["self"]  # don't need `self`
 
 		assert r>=0
-		self.eps = self.args.phi_eps
+		# self.eps = self.args.phi_eps
 
-		self.ci = nn.Parameter(torch.randint(100, r)) # int from 0-100
+		self.ci = nn.Parameter(torch.randint(100, (r-1, 1))) # int from 0-100
 		self.register_parameter("ci_tensor", self.ci)
 
 		self.beta_net = nn.Sequential(
@@ -41,12 +42,13 @@ class Phi(nn.Module):
 
 		# Convert ci to ki
 		ki = torch.tensor([[1]])
-		for i in range(self.r): # A is current coeffs
+		for i in range(self.r-1): # A is current coeffs
 			A = torch.zeros(torch.numel(ki)+1, 2)
 			A[:-1, 0] = ki # copy?
 			A[1:, 1] = ki
 
 			ki = A.mm(torch.tensor([[1], [self.ci[i]]]))
+		# Ultimately, ki is a r-length vector
 
 		# Compute higher-order derivatives of h via finite differencing (sp. forward difference formulation)
 		pasc = pascal(self.r+1, kind='lower') # r+1 because of 0 term
@@ -131,7 +133,7 @@ class Objective(nn.Module):
 
 def main(args):
 	# Boilerplate for saving
-	save_folder = '%s_%s' % (args.dataset, args.affix)
+	save_folder = '%s_%s' % (args.problem, args.affix)
 
 	log_folder = os.path.join(args.log_root, save_folder)
 	model_folder = os.path.join(args.model_root, save_folder)
@@ -145,15 +147,18 @@ def main(args):
 	logger = create_logger(log_folder, 'train', 'info')
 	print_args(args, logger)
 
-	args_savepth = os.join(log_folder, "args.txt")
+	args_savepth = os.path.join(log_folder, "args.txt")
 	save_args(args, args_savepth)
+
+	# print("Parsed and saved args")
+	# IPython.embed()
 
 	# Selecting problem
 	if args.problem == "cartpole":
 		r = 2
 		x_dim = 4
 		u_dim = 1
-		x_lim = np.zeros((x_dim, 2))
+		x_lim = np.array([[-5, 5], [-math.pi/2.0, math.pi/2.0], [-10, 10], [-5, 5]]) # TODO
 
 		# Create phi
 		from src.problems.cartpole import H, XDot, ULimitSetVertices
@@ -169,6 +174,16 @@ def main(args):
 		h_fn = H(param_dict)
 		xdot_fn = XDot(param_dict)
 		uvertices_fn = ULimitSetVertices(param_dict)
+
+		# print("after initializing problem")
+		# bs = 5
+		# x = torch.ones((bs, x_dim))
+		# u = torch.ones((bs, u_dim))
+		# h = h_fn(x)
+		# xdot = xdot_fn(x, u)
+		# uvertices = uvertices_fn(x)
+		# print(h.size(), xdot.size(),uvertices.size())
+		# IPython.embed()
 	else:
 		raise NotImplementedError
 
@@ -177,6 +192,9 @@ def main(args):
 
 	# Create objective function
 	objective_fn = Objective(phi_fn, xdot_fn, uvertices_fn, x_dim, u_dim)
+
+	print("Created CBF and training objective")
+	IPython.embed()
 
 	# Create attacker
 	attacker = BasicAttacker(x_lim)

src/problems/cartpole.py
@@ -15,7 +15,7 @@ class H(nn.Module):
 	def forward(self, x):
 		# TODO (toy): implement
 		# The way these are implemented should be batch compliant
-		rv = torch.abs(x[:, 1]) - self.max_theta
+		rv = torch.abs(x[:, [1]]) - self.max_theta # bs x 1
 		return rv
 
 class XDot(nn.Module):
@@ -28,14 +28,18 @@ class XDot(nn.Module):
 		# x: bs x 4, u: bs x 1
 		# TODO (toy): implement
 		# The way these are implemented should be batch compliant
-		theta = x[:, 1]
-		thetadot = x[:, 3]
+		theta = x[:, [1]]
+		thetadot = x[:, [3]]
 
-		xddot_num = (self.I +self.m*self.l**2)*(self.m*self.l*(thetadot**2)*torch.sin(theta)) - g*(self.m**2)*(self.l**2)*torch.sin(theta)*torch.cos(theta) + (self.I + self.m*(self.l**2))*u
+		xddot_num = (self.I +self.m*(self.l**2))*(self.m*self.l*(thetadot**2)*torch.sin(theta)) - g*(self.m**2)*(self.l**2)*torch.sin(theta)*torch.cos(theta) + (self.I + self.m*(self.l**2))*u
 		denom = self.I*(self.m + self.M) + self.m*(self.l**2)*(self.M + self.m*(torch.sin(theta)**2))
 		thetaddot_num = self.m*self.l*(-self.m*self.l*(thetadot**2)*torch.sin(theta)*torch.cos(theta) + (self.M + self.m)*g*torch.sin(theta)) - self.m*self.l*torch.cos(theta)*u
 
-		rv = torch.cat((xddot_num/denom, thetaddot_num/denom, x[:, [2]], x[:, [3]]), dim=1)
+		# print(xddot_num.size(), denom.size())
+		xddot = xddot_num/denom
+		thetaddot = thetaddot_num/denom
+		# print(xddot.size(), thetaddot.size())
+		rv = torch.cat((x[:, [2]], x[:, [3]], xddot, thetaddot), dim=1)
 		return rv
 
 class ULimitSetVertices(nn.Module):
@@ -47,7 +51,10 @@ class ULimitSetVertices(nn.Module):
 		# TODO (toy): implement
 		# The way these are implemented should be batch compliant
 		# rv is [2, 1]
-		rv = torch.array([[self.max_force], [-self.max_force]])
+		# (bs, n_vertices, u_dim) or (bs, 2, 1)
+		rv = torch.tensor([[self.max_force], [-self.max_force]])
+		rv = rv.unsqueeze(dim=0)
+		rv = rv.expand(x.size()[0], -1, -1)
 		return rv
 
 if __name__ == "__main__":