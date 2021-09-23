fixed weird gradient flow issue

main.py
@@ -44,8 +44,10 @@ class Phi(nn.Module):
 			A[:-1, [0]] = ki
 			A[1:, [1]] = ki
 
-			ki = A.mm(torch.tensor([[1], [self.ci[i]]]))
-
+			# Note: to preserve gradient flow, have to assign to ci not create with ci (i.e. torch.tensor([ci[0]]))
+			binomial = torch.ones((2, 1))
+			binomial[1] = self.ci[i]
+			ki = A.mm(binomial)
 		# Ultimately, ki should be r x 1
 		# print("ci: ", self.ci)
 		# print("ki: ", ki)
@@ -89,8 +91,8 @@ class Objective(nn.Module):
 	def forward(self, x):
 		# The way these are implemented should be batch compliant
 		u_lim_set_vertices = self.uvertices_fn(x) # (bs, n_vertices, u_dim), can be a function of x_batch
-		print("Inside objective's forward function")
-		print(u_lim_set_vertices.size(), u_lim_set_vertices)
+		# print("Inside objective's forward function")
+		# print(u_lim_set_vertices.size(), u_lim_set_vertices)
 
 		n_vertices = u_lim_set_vertices.size()[1]
 
@@ -148,7 +150,7 @@ def main(args):
 		r = 2
 		x_dim = 4
 		u_dim = 1
-		x_lim = np.array([[-5, 5], [-math.pi/2.0, math.pi/2.0], [-10, 10], [-5, 5]]) # TODO
+		x_lim = np.array([[-5, 5], [-math.pi/2.0, math.pi/2.0], [-10, 10], [-5, 5]], dtype=np.float32) # TODO
 
 		# Create phi
 		from src.problems.cartpole import H, XDot, ULimitSetVertices
@@ -208,8 +210,25 @@ def main(args):
 	attacker = BasicAttacker(x_lim)
 
 	# Create test attacker
-	test_attacker = BasicAttacker(x_lim, stop_condition="threshold")
+	test_attacker = BasicAttacker(x_lim, stopping_condition="stop_threshold")
 
+	# TODO: tests
+	# x = attacker.opt(objective_fn, phi_fn)
+	# print(x)
+	# IPython.embed()
+
+
+	# TODO: tests
+	# p_dict = {p[0]: p[1] for p in phi_fn.named_parameters()}
+	# ci = p_dict["ci"]
+	#
+	# x = torch.rand(1, x_dim)
+	# obj_value = objective_fn(x)
+	# ci_grad = grad([obj_value], ci)[0]
+	# print(ci_grad)
+	# IPython.embed()
+
+	# return
 	# Pass everything to Trainer
 	trainer = Trainer(args, logger, attacker, test_attacker)
 	trainer.train(objective_fn, phi_fn, xdot_fn)

src/attacks/basic_attacker.py
@@ -28,63 +28,84 @@ class BasicAttacker():
 	# Want to use Adam, because we want this to converge quickly
 	# How many steps does this take?? If too many, it's going to be an issue.
 	def project(self, phi_fn, x):
-		requires_grad_before = x.requires_grad
-		x.requires_grad = True
+		# requires_grad_before = x.requires_grad
+		# x.requires_grad = True
 
 		# optimizer = optim.Adam([x])
 
+		# TODO: what happens if you can't converge?
 		# Until convergence
-		prev_loss = float("inf")
-		loss = 0
-		while abs(prev_loss - loss) > self.projection_stop_threshold:
-			prev_loss = loss
+		loss = float("inf")
+		while abs(loss) > self.projection_stop_threshold:
+			# IPython.embed()
+			# prev_loss = loss
 
-			# x.grad = None
-			# optimizer.zero_grad()
-			# loss = phi_fn(x)**2
-			# loss.backward()
-			# optimizer.step()
-			# x = x - self.projection_lr*x.grad # should be another lr
-			loss = phi_fn(x)**2
-			grad_to_zero_level = grad([loss], x)[0]
+			x_batch = x.view(1, -1)
+			x_batch.requires_grad = True
+			loss = phi_fn(x_batch)**2
+			grad_to_zero_level = grad([loss], x_batch)[0].squeeze()
+			x_batch.requires_grad = False
+
+			# IPython.embed()
+			# print(grad_to_zero_level)
 			x = x - self.projection_lr*grad_to_zero_level
+			# print(x)
+			# IPython.embed()
 
 			# Clip to bounding box
-			x = torch.clamp(x, min=self.x_lim[:, 0], max=self.x_lim[:, 1])
+			# No torch.clamp in torch 1.7.1
+			x = torch.minimum(torch.maximum(x, torch.tensor(self.x_lim[:, 0])), torch.tensor(self.x_lim[:, 1]))
 
-		x.requires_grad = requires_grad_before
+			# print(grad_to_zero_level)
+			# print(loss)
+			# print(abs(prev_loss - loss))
+
+		# x.requires_grad = requires_grad_before
 		return x
 
 	def step(self, objective_fn, phi_fn, x):
 		# It makes less sense to use an adaptive LR method here, if you think about it
-		requires_grad_before = x.requires_grad
-		x.requires_grad = True
+		# requires_grad_before = x.requires_grad
 
-		obj_grad = grad([-objective_fn(x)], x)[0]
-		normal_to_manifold = grad([phi_fn(x)], x)[0]
+		# IPython.embed()
+		x_batch = x.view(1, -1)
+		x_batch.requires_grad = True
+		obj_val = -objective_fn(x_batch)
+		print(obj_val)
+		phi_val = phi_fn(x_batch)
+		obj_grad = grad([obj_val], x_batch)[0].squeeze()
+		normal_to_manifold = grad([phi_val], x_batch)[0].squeeze()
+		x_batch.requires_grad = False
 
 		proj_obj_grad = obj_grad - torch.dot(obj_grad, normal_to_manifold)*normal_to_manifold
+		# IPython.embed()
 		# Take a step
 		x = x - self.lr*proj_obj_grad
 
 		# Clip to bounding box
 		# Rationale for this step: everytime you take a step on x, clip to box
-		x = torch.clamp(x, min=self.x_lim[:, 0], max=self.x_lim[:, 1])
+
+		# No torch.clamp in torch 1.7.1
+		x = torch.minimum(torch.maximum(x, torch.tensor(self.x_lim[:, 0])), torch.tensor(self.x_lim[:, 1]))
 
 		# Project to surface
 		x = self.project(phi_fn, x)
-		x.requires_grad = requires_grad_before
+
+		# x.requires_grad = requires_grad_before
 		return x
 
 	def opt(self, objective_fn, phi_fn):
 		# Sample 1 point well within box
-		x = torch.rand(self.x_dim)*(self.x_lim[:, 1] - self.x_lim[:, 0]) - self.x_lim[:, 0]
+		x = torch.rand(self.x_dim)*(self.x_lim[:, 1] - self.x_lim[:, 0]) + self.x_lim[:, 0]
 
-		print("This is initial x; is it inside bounds?")
-		print(x, self.x_lim)
+		# print("This is initial x; is it inside bounds?")
+		# print(x, self.x_lim)
 
 		# Project to manifold
 		x = self.project(phi_fn, x)
+
+		# print("ln 85")
+		# IPython.embed()
 
 		should_stop = False
 		# x.requires_grad = True
@@ -103,6 +124,7 @@ class BasicAttacker():
 				should_stop = abs(prev_objective-objective) < self.stop_threshold
 				prev_objective = objective
 
+		# IPython.embed()
 		return x
 
 # TODO: how should .requires_grad be set here?

src/trainer.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from src.utils import save_model
 import os
 import time
-
+import IPython
 """class Trainer:
 	init():
 		In:
@@ -42,35 +42,51 @@ class Trainer():
 		#   call self.attacker
 		#   optimize phi parameters
 
-		# TODO: c_i require projected GD, so you'll need to modify Adam
-		params_no_ci = [tup[1] for tup in phi_fn.named_parameters() if tup[0] != "ci"]
-		ci = phi_fn.get_parameter("ci")
 
+		# TODO: c_i require projected GD, so you'll need to modify Adam
+		p_dict = {p[0]:p[1] for p in phi_fn.named_parameters()}
+		params_no_ci = [tup[1] for tup in phi_fn.named_parameters() if tup[0] != "ci"]
+		ci = p_dict["ci"]
+		# IPython.embed()
+
+		# print("check params_no_ci")
+
+		# TODO
 		optimizer = optim.Adam(params_no_ci)
+		# optimizer = optim.Adam(list(phi_fn.parameters()))
 		_iter = 0
 		prev_test_loss = float("inf")
 		test_loss = 999
-		while abs(prev_test_loss-test_loss) <= self.args.stop_threshold:
+		# while abs(prev_test_loss-test_loss) <= self.args.stop_threshold: # TODO: put this back
+		while _iter < 3:
+			print("inside loop")
+			IPython.embed()
+
 			# Inner min
-			x = self.attacker.opt(self, objective_fn, phi_fn)
+			x = self.attacker.opt(objective_fn, phi_fn)
 
 			# Outer max
 			optimizer.zero_grad()
 			ci.grad = None
 
-			objective_value = objective_fn(x)
-			objective_value.backwards()
+			x_batch = x.view(1, -1)
+			objective_value = objective_fn(x_batch)
+			objective_value.backward()
 
+			print("ci grad: ", ci.grad)
 			optimizer.step()
 			ci = ci + (1e-3)*ci.grad # hardcoded LR
-			ci = torch.clamp(ci, min=torch.zeros_like(ci)) # Project
+			# ci = torch.clamp(ci, min=torch.zeros_like(ci)) # Project
+			# No torch.clamp in torch 1.7.1
+			ci = torch.maximum(ci, torch.zeros_like(ci)) # Project
 
 			# Testing and logging at every iteration
+			prev_test_loss = test_loss
+
 			t1 = time.perf_counter()
 			test_loss = self.test(phi_fn, objective_fn)
 			t2 = time.perf_counter()
 
-			prev_test_loss = test_loss
 			self.logger.info('\n' + '=' * 20 + f' evaluation at iteration: {_iter} ' \
 			            + '=' * 20)
 			self.logger.info(f'test loss: {test_loss:.3f}%, spent: {t2 - t1:.3f} s')