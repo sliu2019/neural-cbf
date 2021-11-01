fixed silly issue; had gradient ASCENT implemented for ci

src/attacks/gradient_batch_attacker.py
@@ -70,17 +70,10 @@ class GradientBatchAttacker():
 			i += 1
 			t_now = time.perf_counter()
 			if torch.max(loss) < self.projection_stop_threshold:
-				# if self.verbose:
-				# 	print(torch.min(loss), torch.max(loss))
-				# 	print("reprojection exited before timeout in %i steps" % i)
-				print("reprojection exited before timeout in %i steps" % i)
+				# print("reprojection exited before timeout in %i steps" % i)
 				break
 			elif (t_now - t1) > self.projection_time_limit:
-				# if self.verbose:
-				# 	print("reprojection exited on timeout")
-				# 	print(torch.min(loss), torch.max(loss))
-				# 	IPython.embed()
-				print("reprojection exited on timeout")
+				# print("reprojection exited on timeout")
 				break
 
 			"""elif (time.perf_counter() - t1) > self.projection_time_limit and (torch.min(loss) <= self.projection_stop_threshold):

src/trainer.py
@@ -38,13 +38,14 @@ class Trainer():
 		timings = [] # in seconds
 		train_attacks = [] # lists of 1D numpy tensors
 		# test_attacks = []
+		ci_lr = 1e-4
 
 		early_stopping = EarlyStopping(patience=self.args.trainer_early_stopping_patience, min_delta=1e-2)
 		while True:
 			# Inner min
 			x = self.attacker.opt(objective_fn, phi_fn)
 
-			# Outer ma
+			# Outer max
 			optimizer.zero_grad()
 			ci.grad = None
 
@@ -57,8 +58,9 @@ class Trainer():
 				IPython.embed()
 
 			optimizer.step()
+
 			with torch.no_grad():
-				new_ci = ci + (1e-3)*ci.grad
+				new_ci = ci - ci_lr*ci.grad
 				new_ci = torch.maximum(new_ci, torch.zeros_like(new_ci)) # Project to all positive
 				ci.copy_(new_ci) # proper way to update
 

throwaway.py
@@ -203,8 +203,10 @@ def plot_2d_binary(checkpoint_number, save_fnm, exp_name):
 
 	phi_fn = Phi(h_fn, xdot_fn, r, x_dim, u_dim, device, args, x_e=x_e)
 
-	plot_range_x = [-0.5, 0.5] # TODO [-math.pi, math.pi]
-	plot_range_y = [-5, -1] # TODO: [-5, 5]
+	# plot_range_x = [-0.5, 0.5] # TODO [-math.pi, math.pi]
+	# plot_range_y = [-5, -1] # TODO: [-5, 5]
+	plot_range_x = [-math.pi, math.pi]
+	plot_range_y = [-5, 5]
 	###################################
 	# IPython.embed()
 	delta = 0.01
@@ -264,11 +266,11 @@ def plot_2d_binary(checkpoint_number, save_fnm, exp_name):
 
 	# Get attacks
 	# TODO: remove, plot recorded attack points
-	# with open("./log/%s/data.pkl" % exp_name, 'rb') as handle:
-	# 	data = pickle.load(handle)
-	# 	train_attacks = data["train_attacks"]
-	# 	test_losses = data["test_losses"]
-	# 	plt.scatter(train_attacks[checkpoint_number][0], train_attacks[checkpoint_number][1], c="tab:orange", marker="x")
+	with open("./log/%s/data.pkl" % exp_name, 'rb') as handle:
+		data = pickle.load(handle)
+		train_attacks = data["train_attacks"]
+		test_losses = data["test_losses"]
+		plt.scatter(train_attacks[checkpoint_number][0], train_attacks[checkpoint_number][1], c="tab:orange", marker="x")
 
 	# TODO: remove, test dS/dG options on batch attacker
 	# logger = create_logger("log/discard", 'train', 'info')
@@ -314,7 +316,7 @@ def plot_3d(checkpoint_number, save_fnm, exp_name):
 
 	x_e = torch.zeros(1, x_dim)
 	phi_fn = Phi(h_fn, xdot_fn, r, x_dim, u_dim, device, args, x_e=x_e)
-	IPython.embed()
+	# IPython.embed()
 	###################################
 	# IPython.embed()
 	delta = 0.1
@@ -638,6 +640,9 @@ if __name__=="__main__":
 	# graph_log_file_2("cartpole_reduced_fixed_attacks_10")
 
 	exp_name = "cartpole_reduced_fixed_attacks_1"
-	for checkpoint_number in np.arange(0, 60, 10):
-		save_fnm = "3d_checkpoint_%i.png" % checkpoint_number
-		plot_3d(checkpoint_number, save_fnm, exp_name)
+	for checkpoint_number in np.arange(0, 50):
+		# save_fnm = "3d_checkpoint_%i.png" % checkpoint_number
+		# plot_3d(checkpoint_number, save_fnm, exp_name)
+
+		save_fnm = "2d_checkpoint_%i.png" % checkpoint_number
+		plot_2d_binary(checkpoint_number, save_fnm, exp_name)