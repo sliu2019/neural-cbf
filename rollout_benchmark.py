a3b54db22e9f0c3134b12931b385dda40727b338
Simin Liu <siminliu@andrew.cmu.edu>
2022-01-12 12:58:09 -0500

    implementationf of rollout experiments complete for our cbf; successfully run on football CBF

--- rollout_benchmark.py
+++ rollout_benchmark.py
@@ -1,4 +1,4 @@
-import scipy as sp 
+import scipy as sp
 import numpy as np 
 import IPython 
 import torch 
@@ -13,11 +13,14 @@ import pickle
 # Note: none of this is batched
 # Everything done in numpy 
 
-exp_name = "cartpole_reduced_debugpinch3_softplus_s1"
+# exp_name = "cartpole_reduced_debugpinch3_softplus_s1" # "football"
+exp_name = "cartpole_reduced_debugpinch1_softplus_s1" # "baguette"
 checkpoint_number = 1450
 
 # todo: LATER, you can load all the below from the experiment name
-dt = 0.005 # TODO
+# dt = 0.005 # TODO
+# dt = 1e-8
+dt = 1e-5
 
 g = 9.81
 I = 1.2E-3
@@ -47,7 +50,7 @@ def numpy_phi_fn(x):
 
 	phi_output = phi_fn(x_input)
 	# phi_vals = phi_output[0,-1].item()
-	phi_vals = phi_output.detach().cpu().numpy()
+	phi_vals = phi_output.detach().cpu().numpy().flatten()
 	return phi_vals
 
 def numpy_phi_grad(x):
@@ -83,19 +86,19 @@ def compute_u_ours(t, x):
 	qp_rhs = None
 	################
 
-	phi_vals = numpy_phi_fn(x)
+	phi_vals = numpy_phi_fn(x) # This is an array of (1, r+1), where r is the degree
 	phi_grad = numpy_phi_grad(x)
 
-	x_next = x + dt*x_dot_open_loop(x, compute_u_ref(t, x))
+	x_next = x + dt*x_dot_open_loop(x, compute_u_ref(t, x)) # in the absence of safe control, the next state
 	next_phi_val = numpy_phi_fn(x_next)
 
-	if phi_vals[0, -1] > 0:
+	if phi_vals[-1] > 0: # Outside
 		eps = 5.0 # TODO
 		apply_u_safe = True
-	elif phi_vals[0, -1] < 0 and next_phi_val[0, -1] >= 0: # Note: cheating way to convert DT to CT
+	elif phi_vals[-1] < 0 and next_phi_val[-1] >= 0: # On boundary. Note: cheating way to convert DT to CT
 		eps = 1.0 # TODO
 		apply_u_safe = True
-	else:
+	else: # Inside
 		apply_u_safe = False
 		debug_dict = {"apply_u_safe": apply_u_safe, "u_ref": u_ref, "qp_slack": qp_slack, "qp_rhs":qp_rhs, "qp_lhs":qp_lhs, "phi_vals":phi_vals}
 		return u_ref, debug_dict
@@ -116,16 +119,19 @@ def compute_u_ours(t, x):
 	qp_rhs = rhs.item()
 	Q = 2*np.array([[1.0, 0], [0, 0]])
 	p = np.array([[-2.0*u_ref], [w]])
-	G = np.array([[lhs, -1.0], [1, 0], [-1, 0], [0, -1]])
-	h = np.array([[rhs], [max_force], [max_force], [0.0]])
+	G = np.array([[qp_lhs, -1.0], [1, 0], [-1, 0], [0, -1]])
+	h = np.array([[qp_rhs], [max_force], [max_force], [0.0]])
 
+	# try:
 	sol_obj = solvers.qp(matrix(Q), matrix(p), matrix(G), matrix(h))
+	# except:
+	# 	IPython.embed()
 	sol_var = sol_obj['x']
 
 	u_safe = sol_var[0]
 	qp_slack = sol_var[1]
 
-	debug_dict = {"apply_u_safe": apply_u_safe, "u_ref": u_ref, "qp_slack": qp_slack, "qp_rhs":qp_rhs, "qp_lhs":qp_lhs, "phi_vals":phi_vals}
+	debug_dict = {"apply_u_safe": apply_u_safe, "u_ref": u_ref, "qp_slack": qp_slack, "qp_rhs":qp_rhs, "qp_lhs":qp_lhs, "phi_vals":phi_vals.flatten()}
 	return u_safe, debug_dict
 
 def x_dot_open_loop(x, u):
@@ -153,18 +159,21 @@ def x_dot_closed_loop(t, x):
 	x_dot = x_dot_open_loop(x, u)
 	return x_dot
 
-def simulate_rollout(x0, T_max=100):
+def simulate_rollout(x0, N_dt=100):
+	# print("Inside simulate_rollout")
+	# IPython.embed()
+
 	x = x0.copy()
 	# 	debug_dict = {"apply_u_safe": apply_u_safe, "u_ref": u_ref, "qp_slack": qp_slack, "qp_rhs":qp_rhs, "qp_lhs":qp_lhs, "phi_vals":phi_vals}
 	xs = [x]
 	us = []
 	dict = None
 
-	for t in range(T_max):
-		u, debug_dict = compute_u_ours(t, x)
+	for t in range(N_dt):
+		u, debug_dict = compute_u_ours(t, x) # Define this
 		x_dot = x_dot_open_loop(x, u)
 		x = x + dt*x_dot
-
+  
 		us.append(u)
 		xs.append(x)
 
@@ -172,14 +181,23 @@ def simulate_rollout(x0, T_max=100):
 			# dict = debug_dict
 			dict = {key:[value] for (key, value) in debug_dict.items()}
 		else:
-			dict = {key:(value.append(debug_dict[key])) for (key, value) in debug_dict.items()}
+			# IPython.embed()
+			for key, value in dict.items():
+				# if t == 1:
+				# 	IPython.embed()
+				# print(key, value)
+				value.append(debug_dict[key])
+				# dict[key] = value
+				# print(new_value, type(new_value))
+			# dict = {key:(value.append(debug_dict[key])) for (key, value) in dict.items()}
+		# print(dict)
 
 	dict = {key:np.array(value) for (key, value) in dict.items()}
 	dict["x"] = np.array(xs)
 	dict["u"] = np.array(us)
 
-	print("At the end of a rollout")
-	IPython.embed()
+	# print("At the end of a rollout")
+	# IPython.embed()
 	return dict
 
 def compute_phi_signs():
@@ -194,24 +212,34 @@ def compute_phi_signs():
 	input = torch.from_numpy(input)
 	phi_vals = phi_fn(input)
 
-	S_vals = torch.max(phi_vals, dim=1)[0] # S = all phi_i <= 0
-	phi_signs = torch.sign(S_vals).detach().cpu().numpy()
-	phi_signs = np.reshape(phi_signs, X.shape)
-
-	return phi_signs, X, Y # square array with 1, -1. Negative indicates inside invariant set
+	phi_vals = phi_vals.detach().cpu().numpy()
+	return phi_vals, X, Y # square array with 1, -1. Negative indicates inside invariant set
 
 if __name__ == "__main__":
 	log_folder = "debug"
-	log_fname = "our_cbf"
+	log_fname = "our_cbf_baguette"
 
 	makedirs(os.path.join("rollouts", log_folder))
 
+	# IPython.embed()
 	N_rollout = 5 # TODO
-	phi_signs, X, Y = compute_phi_signs()
+	T_max = 1.0 # seconds def: 100
+	N_dt = int(T_max/dt)
+	phi_vals_on_grid, X, Y = compute_phi_signs()
+	# S_vals = torch.max(phi_vals, dim=1)[0] # S = all phi_i <= 0
+	# phi_signs = torch.sign(S_vals).detach().cpu().numpy()
+	# phi_signs = np.reshape(phi_signs, X.shape)
+	S_vals = phi_vals_on_grid.max(axis=1) # S = all phi_i <= 0
+	phi_signs = np.sign(S_vals)
+	phi_signs = np.reshape(phi_signs, X.shape)
+
 	where_invariant = np.argwhere(phi_signs == -1)
 	which = np.random.choice(np.arange(where_invariant.shape[0]), size=N_rollout, replace=False)
 	chosen_invariant_ind = where_invariant[which]
-	chosen_invariant = np.concatenate((np.reshape(X[chosen_invariant_ind[:, 0]], (-1, 1)), np.reshape(Y[chosen_invariant_ind[:, 1]], (-1, 1))), axis=1)
+	chosen_X = X[chosen_invariant_ind[:, 0], chosen_invariant_ind[:, 1]]
+	chosen_Y = Y[chosen_invariant_ind[:, 0], chosen_invariant_ind[:, 1]]
+
+	chosen_invariant = np.concatenate((chosen_X[:, None], chosen_Y[:, None]), axis=1)
 
 	x0s = np.zeros((N_rollout, 4))
 	x0s[:, 1] = chosen_invariant[:, 0]
@@ -227,11 +255,11 @@ if __name__ == "__main__":
 	plt.savefig("./rollouts/%s/%s_x0.png" % (log_folder, log_fname), bbox_inches='tight')
 	plt.clf()
 	plt.close()
-	IPython.embed()
+	# IPython.embed()
 
 	info_dicts = None
 	for i in range(N_rollout):
-		info_dict = simulate_rollout(x0s[i])
+		info_dict = simulate_rollout(x0s[i], N_dt)
 
 		if info_dicts is None:
 			info_dicts = info_dict
@@ -246,24 +274,69 @@ if __name__ == "__main__":
 		pickle.dump(info_dicts, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 	# Sanity checks
-	print("before sanity checks")
-	IPython.embed()
+	# print("before sanity checks")
+	# IPython.embed()
 	# 1. Check that all rollouts touched the invariant set boundary. If not, increase T_max
 	# 2. Compute the number of exits for each rollout
 	# 3. Compute the number of rollouts without any exits
 	# debug_dict = {"x": x, "u": u, "apply_u_safe": apply_u_safe, "u_ref": u_ref, "qp_slack": qp_slack, "qp_rhs":qp_rhs, "qp_lhs":qp_lhs, "phi_vals":phi_vals}
+	print("*********************************************************\n")
 	apply_u_safe = info_dicts["apply_u_safe"]  # (N_rollout, T_max)
 	rollouts_any_safe_ctrl = np.any(apply_u_safe, axis=1)
-	print(np.any(rollouts_any_safe_ctrl))
-	print(rollouts_any_safe_ctrl)
+	print("Did we apply safe control?" , np.all(rollouts_any_safe_ctrl))
+	if np.all(rollouts_any_safe_ctrl) == False:
+		print("Which rollouts did we apply safe control?", rollouts_any_safe_ctrl)
+		false_ind = np.argwhere(np.logical_not(rollouts_any_safe_ctrl))
+		x = info_dicts["x"]
+		x_for_false = x[false_ind.flatten()]
+		theta_for_false = x_for_false[:, :, 1]
+		thetadot_for_false = x_for_false[:, :, 3]
+
 
 	phi_vals = info_dicts["phi_vals"] # (N_rollout, T_max, r+1)
 	phi_max = np.max(phi_vals, axis=2)
 	rollouts_any_exits = np.any(phi_max > 0, axis=1)
-	print("Any exits?", np.any(rollouts_any_exits))
-	print("Percent exits: ", np.mean(rollouts_any_exits))
-	print("Which rollouts have exits:", rollouts_any_exits)
-	print("How many exists per rollout: ", np.sum(phi_max > 0, axis=1))
+	any_exits = np.any(rollouts_any_exits)
+	print("Any exits?", any_exits)
+	if any_exits:
+		print("Percent exits: ", np.mean(rollouts_any_exits))
+		print("Which rollouts have exits:", rollouts_any_exits)
+		print("How many exits per rollout: ", np.sum(phi_max > 0, axis=1))
 
-	print("after sanity checks")
+	phi_star = phi_vals[:, :, -1]
+	rollouts_any_phistar_pos = np.any(phi_star>0, axis=1)
+	any_phistar_pos = np.any(rollouts_any_phistar_pos)
+
+	print("Any phi_star positive?", any_phistar_pos)
+	if any_phistar_pos:
+		print("Which rollouts had phi_star positive:", rollouts_any_phistar_pos)
+
+	# print("after sanity checks")
+	# # phi_vals (20, 100, 1, 3)
+	#
+	# for key, value in info_dicts.items():
+	# 	print(key, value.shape)
+
+	# Plot trajectories
+	print("Check plotted trajectories")
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.imshow(phi_signs, extent=x_lim.flatten())
+	ax.set_aspect("equal")
+	ax.scatter(chosen_invariant[:, 0], chosen_invariant[:, 1])
+
+	# phi_vals_numpy = phi_vals[:, -1].detach().cpu().numpy()
+	# IPython.embed()
+	phi_star_on_grid = phi_vals_on_grid[:, -1]
+	plt.contour(X, Y, np.reshape(phi_star_on_grid, X.shape), levels=[0.0],
+	                 colors=('k',), linewidths=(2,))
+
+	x = info_dicts["x"]
+	for i in range(N_rollout):
+		x_rl = x[i]
+		plt.plot(x_rl[:, 1], x_rl[:, 3])
+	plt.savefig("./rollouts/%s/%s_trajectories.png" % (log_folder, log_fname), bbox_inches='tight')
+	plt.clf()
+	plt.close()
+
 	IPython.embed()
