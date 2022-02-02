9fcd5d04eca2b51cdad2c82c94b84dff1ce84325
Simin Liu <siminliu@andrew.cmu.edu>
2022-02-02 17:55:15 -0500

    first attempt at fixing ulimitsetvertices for new dynamics

--- src/problems/flying_inv_pend.py
+++ src/problems/flying_inv_pend.py
@@ -11,16 +11,15 @@ class H(nn.Module):
 	def __init__(self, param_dict):
 		super().__init__()
 		self.__dict__.update(param_dict)  # __dict__ holds and object's attributes
-		self.i = self.index_dict
+		self.i = self.state_index_dict
 
 	def forward(self, x):
 		# TODO (toy): implement
 		# The way these are implemented should be batch compliant
 		# Return value is size (bs, 1)
-
 		theta = x[:, [self.i["theta"]]]
 		phi = x[:, [self.i["phi"]]]
-		rv = torch.acos(torch.cos(theta)*torch.sin(phi)) - self.delta_safety_limit # TODO: torch.acos output is in [0, 2pi]?
+		rv = torch.acos(torch.cos(theta)*torch.cos(phi)) - self.delta_safety_limit # TODO: torch.acos output is in [0, 2pi]? Probably doesn't matter for our purposes
 
 		return rv
 
@@ -30,10 +29,10 @@ class XDot(nn.Module):
 		self.__dict__.update(param_dict)  # __dict__ holds and object's attributes
 		# IPython.embed()
 		self.device = device
-		self.i = self.index_dict
+		self.i = self.state_index_dict
 
 	def forward(self, x, u):
-		# x: bs x 13, u: bs x 4
+		# x: bs x ?, u: bs x 4
 		# The way these are implemented should be batch compliant
 
 		# Pre-computations
@@ -64,16 +63,11 @@ class XDot(nn.Module):
 		k_y = R[:, 1, 2]
 		k_z = R[:, 2, 2]
 
-		F = u[:, 0]
+		F = (u[:, 0] + self.M*g) # TODO: Mg
 		# tau_gamma = u[:, 1]
 		# tau_beta = u[:, 2]
 		# tau_alpha = u[:, 3]
 		###### Computing state derivatives
-		# IPython.embed()
-		ddx = (1.0/self.M)*k_x*F
-		ddy = (1.0/self.M)*k_y*F
-		ddz = (1.0/self.M)*k_z*F - g
-
 		ddquad_angles = torch.bmm(R, u[:, 1:, None]) # (N, 3, 1)
 		ddquad_angles = ddquad_angles[:, :, 0]
 		ddgamma = (1.0/self.J_x)*ddquad_angles[:, 0]
@@ -84,7 +78,8 @@ class XDot(nn.Module):
 		ddtheta = (3.0*(-k_x*torch.cos(theta)-k_y*torch.sin(phi)*torch.sin(theta) + k_z*torch.cos(phi)*torch.sin(theta))/(2.0*self.M*self.L_p))*F - torch.square(dphi)*torch.sin(theta)*torch.cos(theta)
 
 		# rv = torch.cat([x[:, [self.i["dx"]]], x[:, [self.i["dy"]]], x[:, [self.i["dz"]]], ddx[:, None], ddy[:, None], ddz[:, None], x[:, [self.i["dgamma"]]], x[:, [self.i["dbeta"]]], x[:, [self.i["dalpha"]]], ddgamma[:, None], ddbeta[:, None], ddalpha[:, None], dphi[:, None], dtheta[:, None], ddphi[:, None], ddtheta[:, None]], axis=1)
-		rv = torch.cat([ddx[:, None], ddy[:, None], ddz[:, None], x[:, [self.i["dgamma"]]], x[:, [self.i["dbeta"]]], x[:, [self.i["dalpha"]]], ddgamma[:, None], ddbeta[:, None], ddalpha[:, None], dphi[:, None], dtheta[:, None], ddphi[:, None], ddtheta[:, None]], axis=1)
+		# Excluding translational motion
+		rv = torch.cat([x[:, [self.i["dgamma"]]], x[:, [self.i["dbeta"]]], x[:, [self.i["dalpha"]]], ddgamma[:, None], ddbeta[:, None], ddalpha[:, None], dphi[:, None], dtheta[:, None], ddphi[:, None], ddtheta[:, None]], axis=1)
 		return rv
 
 class ULimitSetVertices(nn.Module):
@@ -105,11 +100,65 @@ class ULimitSetVertices(nn.Module):
 		r3 = np.concatenate((np.zeros(2), np.ones(2),np.zeros(2), np.ones(2), np.zeros(2), np.ones(2),np.zeros(2), np.ones(2)))
 		r4 = np.zeros(16)
 		r4[1::2] = 1.0
-		impulse_vert = np.concatenate((r1[None], r2[None], r3[None], r4[None]), axis=0) # 8 vertices in the impulse control space
+		impulse_vert = np.concatenate((r1[None], r2[None], r3[None], r4[None]), axis=0) # 16 vertices in the impulse control space
 
-		force_vert = M@impulse_vert
-		force_vert = force_vert.T.astype("float32") # necessary for GPU + PyTorch
+		# TODO: biasing the force down by Mg makes computing the limit set a little complicated
+		# F (thrust) is an input that has constraints; must be positive
+		# U_i have constraints as well, but Torques don't
+		# So after shifting the polyhedron down by Mg on the F axis, we also have to compute any vertices appearing at the intersection of this polyhedron and the 0-plane
+		"""
+		force_vert = M@impulse_vert 
+		force_vert = force_vert.T.astype("float32")
 		self.vert = torch.from_numpy(force_vert).to(self.device)
+		"""
+		# IPython.embed()
+
+		force_vert = M@impulse_vert + np.array([[-self.M*g], [0],[0], [0]]) # TODO: Mg # (4, 16)
+		# force_vert = force_vert.T.astype("float32")
+		# self.vert = torch.from_numpy(force_vert).to(self.device)
+
+		# Go through all the edges and check if they intersect with the plane. If so, find the intersection.
+		# Affine transformations preserve edges (as well as vertices)
+		# We'll go through the edges in U-space. Each U-vertex has 4 neighbors.
+		# Naturally, we will be double-computing the plane-poly intersection vertices.
+		all_cross_points = []
+
+		for i in range(16):
+			v = impulse_vert[:, [i]]
+			neighbors = (v + np.eye(4)) % 2
+			transf_v = M@v + np.array([[-self.M*g], [0],[0], [0]])
+			transf_neighbors = M@neighbors + np.array([[-self.M*g], [0],[0], [0]])
+
+			# Does edge cross F = 0 plane?
+			ind = np.argwhere(transf_v[0, 0]*transf_neighbors[0] < 0).flatten()
+			if ind.size:
+				# IPython.embed()
+				crossing_neigh = transf_neighbors[:, ind]
+
+				print(transf_v, crossing_neigh)
+				# Find where they intersect
+				alphas = -transf_v[0]/(crossing_neigh-transf_v)[0, :]
+				cross_point = transf_v + alphas*(crossing_neigh-transf_v)
+				all_cross_points.extend(list(cross_point.T))
+
+		# print(all_cross_points)
+		# IPython.embed()
+		# Set
+		all_cross_points_set = {tuple(cross_point) for cross_point in all_cross_points}
+		all_cross_points = np.array([np.array(x) for x in all_cross_points_set])
+
+		pos_vert_ind = np.argwhere(force_vert[0, :]>=0).flatten()
+		pos_vert = force_vert[:, pos_vert_ind].T # (4, whatever)
+
+		all_vert = np.concatenate((all_cross_points, pos_vert), axis=0) # (25, 4)
+
+		# Processing for Pytorch
+		all_vert = all_vert.astype("float32")
+		self.vert = torch.from_numpy(all_vert).to(self.device)
+
+		# force_vert = force_vert.T.astype("float32")
+		# self.vert = torch.from_numpy(force_vert).to(self.device)
+		# TODO: add pos from force_vert to cross_points
 
 	def forward(self, x):
 		# TODO (toy): implement
@@ -141,13 +190,12 @@ if __name__ == "__main__":
 	}
 	param_dict["M"] = param_dict["m"] + param_dict["m_p"]
 
-	# IPython.embed()
-	index_names = ["dx", "dy", "dz", "gamma", "beta", "alpha", "dgamma", "dbeta", "dalpha", "phi", "theta", "dphi", "dtheta"] # excluded x, y, z
-	index_dict = dict(zip(index_names, np.arange(len(index_names))))
-	param_dict["index_dict"] = index_dict
+	state_index_names = ["gamma", "beta", "alpha", "dgamma", "dbeta", "dalpha", "phi", "theta", "dphi", "dtheta"] # excluded x, y, z
+	state_index_dict = dict(zip(state_index_names, np.arange(len(state_index_names))))
+	param_dict["state_index_dict"] = state_index_dict
 	##############################################
 
-	"""if torch.cuda.is_available():
+	if torch.cuda.is_available():
 		os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 		dev = "cuda:%i" % (0)
 		print("Using GPU device: %s" % dev)
@@ -165,9 +213,9 @@ if __name__ == "__main__":
 	u = torch.rand(N, 4)
 
 	# IPython.embed()
-	rv1 = h_fn(x)
+	# rv1 = h_fn(x)
 	# rv2 = xdot_fn(x, u)
-	# rv3 = uvertices_fn(x)
+	rv3 = uvertices_fn(x)
 
 	# print(rv1.shape)
-	# IPython.embed()"""
+	# IPython.embed()
