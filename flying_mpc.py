fixed bug in softplus loss calculation with nan

flying_mpc.py
@@ -203,6 +203,7 @@ def mpc_compute_invariant_set(args):
 	# print(save_fpth_root)
 	# print("ln 220")
 	# IPython.embed()
+	t_per_mpc = []
 	for i in range(n_neg_inds):
 		# print("inside loop")
 		# IPython.embed()
@@ -234,6 +235,7 @@ def mpc_compute_invariant_set(args):
 		"""Please choose from dict_keys(['_time', '_x', '_y', '_u', '_z', '_tvp', '_p', '_aux', '_eps', 'opt_p_num', '_opt_x_num', '_opt_aux_num', '_lam_g_num', 'success', 't_wall_total'])"""
 		t1 = time.perf_counter()
 		print("t for mpc on a single x0: %.3f s" % (t1 -t0))
+		t_per_mpc.append((t1-t0))
 
 	# Save data
 	# print("line 251")
@@ -241,7 +243,7 @@ def mpc_compute_invariant_set(args):
 	S_grid = np.zeros_like(X)
 	S_grid[neg_inds[:, 0], neg_inds[:, 1]] = exists_soln_bools
 	A_grid = (phi_0_vals <= 0).astype("int")
-	save_dict = {"A_grid": A_grid, "X": X, "Y": Y, "exists_soln_bools": exists_soln_bools, "S_grid": S_grid, "args": args}
+	save_dict = {"A_grid": A_grid, "X": X, "Y": Y, "exists_soln_bools": exists_soln_bools, "S_grid": S_grid, "args": args, "t_per_mpc": t_per_mpc}
 	with open(save_fpth_root + ".pkl", 'wb') as handle:
 		pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 

src/trainer.py
@@ -108,6 +108,8 @@ class Trainer():
 		file_name = os.path.join(self.args.model_folder, f'checkpoint_{_iter}.pth')
 		save_model(phi_fn, file_name)
 
+		# print("Before training")
+		# IPython.embed()
 		while True:
 
 			iteration_info_dict = {}
@@ -127,16 +129,27 @@ class Trainer():
 			elif self.args.objective_option == "weighted_average":
 				c = 0.1
 
+				# obj = objective_fn(X)
+				# mask_neg = obj >= 0 # zeros out entries where obj < 0: actually, just use softplus on the objective
+				# with torch.no_grad():
+				# 	if torch.any(mask_neg):
+				# 		w = torch.exp(c*obj)
+				# 		w = w*mask_neg
+				# 		w = w/torch.sum(w)
+				# 	else:
+				# 		w = torch.zeros_like(obj)
+				# attack_value = torch.dot(w.flatten(), obj.flatten())
+				# TODO: above implementation will fail when obj contains large values (>1000).
+				# torch.exp overflows easily; torch.nn.functional.softmax is way better
+
+				# print("Is gradient propagated correctly? Through indexing?")
+				# IPython.embed()
 				obj = objective_fn(X)
-				mask_neg = obj >= 0 # zeros out entries where obj < 0: actually, just use softplus on the objective
+				pos_inds = torch.where(obj >= 0) # tuple of 2D inds
+				pos_obj = c*obj[pos_inds[0], pos_inds[1]]
 				with torch.no_grad():
-					if torch.any(mask_neg):
-						w = torch.exp(c*obj)
-						w = w*mask_neg
-						w = w/torch.sum(w)
-					else:
-						w = torch.zeros_like(obj)
-				attack_value = torch.dot(w.flatten(), obj.flatten())
+					w = torch.nn.functional.softmax(pos_obj)
+				attack_value = torch.dot(w.flatten(), pos_obj.flatten())
 			elif self.args.objective_option == "weighted_average_include_neg_phidot":
 				# Eliminates the "relu" effect on above
 				c = 0.1
@@ -275,6 +288,8 @@ class Trainer():
 				with open(self.data_save_fpth, 'wb') as handle:
 					pickle.dump(data_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+			# print("After first training iteration")
+			# IPython.embed()
 			#######################################################
 			##############   Compute test stats   #################
 			#######################################################