caught bug? weighted average loss implemented incorrectly in trainer.py

src/trainer.py
@@ -12,7 +12,7 @@ import datetime
 import pickle
 import math
 
-from torch.optim.lr_scheduler import ReduceLROnPlateau, ExponentialLR
+# from torch.optim.lr_scheduler import ReduceLROnPlateau, ExponentialLR
 # lr_scheduler_str_to_class_dict = {"exponential_reduction":0, "reduce_on_plateau":0}
 
 from src.phi_designs.neural_phi import NeuralPhi
@@ -120,6 +120,8 @@ class Trainer():
 			x, debug_dict = self.attacker.opt(objective_fn, phi_fn, _iter, debug=True)
 			X = debug_dict["X_final"]
 
+			optimizer.zero_grad()
+
 			if self.args.objective_option == "regular":
 				x_batch = x.view(1, -1)
 				attack_value = objective_fn(x_batch)[0, 0]
@@ -127,20 +129,32 @@ class Trainer():
 				x_batch = x.view(1, -1)
 				attack_value = nn.functional.softplus(objective_fn(x_batch)[0, 0])
 			elif self.args.objective_option == "weighted_average":
+				# obj = objective_fn(X)
+				# mask_neg = obj >= 0 # zeros out entries where obj < 0: actually, just use softplus on the objective
+				# with torch.no_grad():
+				# 	if torch.any(mask_neg):
+				# 		w = torch.exp(c*obj)
+				# 		w = w*mask_neg
+				# 		w = w/torch.sum(w)
+				# 	else:
+				# 		w = torch.zeros_like(obj)
+				# attack_value = torch.dot(w.flatten(), obj.flatten())
+				# TODO: above implementation will fail when obj contains large values (>1000).
+				# torch.exp overflows easily; torch.nn.functional.softmax is way better
+
 				c = 0.1
 				obj = objective_fn(X)
 				pos_inds = torch.where(obj >= 0) # tuple of 2D inds
-				pos_obj = c*obj[pos_inds[0], pos_inds[1]]
+				pos_obj = obj[pos_inds[0], pos_inds[1]]
 				with torch.no_grad():
-					w = torch.nn.functional.softmax(pos_obj)
+					w = torch.nn.functional.softmax(c*pos_obj)
 				attack_value = torch.dot(w.flatten(), pos_obj.flatten())
 			elif self.args.objective_option == "weighted_average_include_neg_phidot":
 				# Eliminates the "relu" effect on above
 				c = 0.1
-
 				obj = objective_fn(X)
-				w = torch.exp(c*obj)
-				w = w/torch.sum(w)
+				with torch.no_grad():
+					w = torch.nn.functional.softmax(c*obj)
 				attack_value = torch.dot(w.flatten(), obj.flatten())
 
 			# For logging
@@ -152,8 +166,6 @@ class Trainer():
 			#######################################################
 			############# Now, taking the gradients ###############
 			#######################################################
-			optimizer.zero_grad()
-
 			reg_value.backward()
 
 			##### Check reg gradient #####